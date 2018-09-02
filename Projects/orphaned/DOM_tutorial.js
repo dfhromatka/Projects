e53bdbf3c815ade105830352e90288de3526f11f
dfhromatka <dfhromatka@users.noreply.github.com>
2018-09-01 22:04:46 -0500

    Add files via upload

---
+++ Projects/orphaned/DOM_tutorial.js
@@ -0,0 +1,9 @@
+console.log(document.domain);
+console.log(document.URL)
+console.log(document.type)
+console.log(document.head)
+console.log(document.body)
+
+console.log(document.doctype)
+console.log(document.all)
+console.log(document.title)
